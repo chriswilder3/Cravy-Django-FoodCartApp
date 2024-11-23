fbd8518fa7e2974ecfc8e7d7cf0891c265bddf31
chriswilder3 <chriswilderdelta19@gmail.com>
2024-11-24 01:47:41 +0530

    improved navbar, detail page, added logo,

--- food/urls.py
+++ food/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'food' # This is used to refer to names of paths 
+                 # in combination with it, to avoid conflicts with
+                 # possible same names from other apps.
+
+                 # We can refer the names now as food.name_of_url_pattern
+                 # in the template. Ex :{% url 'food:item' %}
+                 # Remember its :(colon) not .
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('item/', views.item, name = 'item'),
