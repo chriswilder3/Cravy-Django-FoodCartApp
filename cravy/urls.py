a2875cbaa8c76f97f8f9222b4aaef0505f534171
chriswilder3 <chriswilderdelta19@gmail.com>
2024-11-24 17:08:24 +0530

    added image fields

--- cravy/urls.py
+++ cravy/urls.py
@@ -17,7 +17,34 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+    # The configurations defined in settings.py are available
+    # in this module. Ex: MEDIA_URL, STATIC_ROOT etc
+
+from django.conf.urls.static import static
+    # static function is a tool provided by django to serve
+    # static and media files during DEVELOPMENT.
+    # As we know, the media_root is where media files are 
+    # stored. When user requests for these images/media,it happens
+    # through URLs, ex : src attr of img tag. Now 
+    # static function is used to append additional
+    # URL patterns of media access just like path() does.
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('food/', include('food.urls'))
+    path('', include('food.urls')),
 ]
+
+if settings.DEBUG:
+    # Note that this configuration of serving media files by django
+    # itself is active during development. While deployed, the
+    # servers like apache or nginx themselves serve files from file system.
+
+    urlpatterns += static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
+    # As we might notice, the function is very similar to path()
+    # defined in urlpatterns at top. Because it works same way.
+    # Whenever user requests for media with given url = MEDIA_URL
+    # The path corresponding to that media is served , so 
+    # That the media is displayed back to user/template.
--- food/models.py
+++ food/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     name = models.CharField(max_length= 255)
     description = models.CharField(max_length= 255)
     price = models.IntegerField()
+    image = models.ImageField(upload_to = 'product_images/', null= True)
 
     def __str__(self):
         return f" {self.name}"
